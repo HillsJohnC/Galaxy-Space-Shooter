92e38408825be392c4f692fb66aa4d7684b23faf
John Hills <hillsjohnc@yahoo.com>
2022-05-02 21:38:19 -0500

    Created Enemy and Enemy Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move down at 4m/sec
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // If bottom of screen
+        // Respwn at top with a new random x position.
+
+        if (transform.position.y < -5f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);
+        }
+    }
+}
